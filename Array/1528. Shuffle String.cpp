2f16765add302807edff8a13d0232c5819677015
Gaurav14cs17 <gouravkumar717@gmail.com>
2022-06-03 21:55:17 +0530

    array

---
+++ Array/1528. Shuffle String.cpp
@@ -0,0 +1,22 @@
+/*
+https://leetcode.com/problems/shuffle-string/
+*/
+
+class Solution {
+public:
+    string restoreString(string s, vector<int>& indices) {
+        int n = indices.size();
+        vector<char>temp_str(n);
+
+        for(int i =0  ; i<n ; ++i){
+           temp_str[indices[i]] = s[i];
+        }
+
+        
+        string ans = "";
+        for(auto it : temp_str)
+            ans = ans + it;
+        //cout<<ans<<endl;
+        return ans;
+    }
+};
