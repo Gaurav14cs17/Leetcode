53aa2682a91ed7b64fb6bea9859daf833d967ec4
Gaurav14cs17 <gouravkumar717@gmail.com>
2022-06-04 23:52:12 +0530

    Array

---
+++ Array/1732. Find the Highest Altitude.cpp
@@ -0,0 +1,25 @@
+/*
+https://leetcode.com/problems/find-the-highest-altitude/
+*/
+
+class Solution {
+public:
+    const int N = 5e5 +5;
+
+    int largestAltitude(vector<int>& gain) {
+        int dp[N];
+        int n = gain.size();
+
+        dp[0] = 0;
+        int mx = 0;
+
+        for(int i = 1 ;i<=n ; ++i  ){
+            dp[i] = dp[i-1] + gain[i-1];
+            mx =  max(mx , dp[i]);
+        }
+
+
+        return mx;
+
+    }
+};
