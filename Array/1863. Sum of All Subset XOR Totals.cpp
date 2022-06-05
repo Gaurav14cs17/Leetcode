37f3a40a524937fb570ad500764066f12424ec0f
Gaurav14cs17 <gouravkumar717@gmail.com>
2022-06-05 21:48:36 +0530

    Array

--- Array/1863. Sum of All Subset XOR Totals.cpp
+++ Array/1863. Sum of All Subset XOR Totals.cpp
@@ -2,12 +2,13 @@
 https://leetcode.com/problems/sum-of-all-subset-xor-totals/
 */
 
+Soln 1:
+
 class Solution {
 public:
     int subsetXORSum(vector<int>& nums) {
         int n = nums.size();
         int ans =0;
-
         for(int bits =0 ; bits <(1<<n) ; ++bits ){
             int s =0;
             for(int i =0 ; i<n ; ++i ){
@@ -16,6 +17,32 @@ public:
             }
             ans += s;
         }
+        return ans;
+    }
+};
+
+
+Soln : 2
+
+
+class Solution {
+public:
+
+    void solve(vector<int>&nums , int idx  ,int n , int val , int &cur  ){
+
+        if(idx==n){
+            cur += val;
+            return ;
+        }
+
+        solve( nums , idx +1 , n , val ^ nums[idx]  , cur );
+        solve( nums , idx +1 , n , val , cur );
+    }
+
+    int subsetXORSum(vector<int>& nums) {
+        int n = nums.size();
+        int ans =0;
+        solve( nums , 0 , n , 0 , ans);
         return ans;
     }
 };
