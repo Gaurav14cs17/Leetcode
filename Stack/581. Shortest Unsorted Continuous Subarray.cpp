c6e89e42ed558d699e365d889b8179acd8709e37
Gaurav14cs17 <gouravkumar717@gmail.com>
2022-06-20 12:20:22 +0530

    stack

---
+++ Stack/581. Shortest Unsorted Continuous Subarray.cpp
@@ -0,0 +1,32 @@
+/*
+https://leetcode.com/problems/shortest-unsorted-continuous-subarray/
+*/
+
+class Solution {
+public:
+    int findUnsortedSubarray(vector<int>& nums) {
+        int n = nums.size();
+
+        vector<int>v = nums;
+        int l = 0;
+        int r = n-1;
+        sort(v.begin() , v.end());
+
+        while(l < r ){
+            if(v[l]==nums[l]){
+                ++l;
+            }
+            else if(v[r] == nums[r]){
+                --r;
+            }
+            else {
+                break;
+            }
+        }
+
+        if( r > l )
+            return r- l +1;
+
+        return 0;
+    }
+};
