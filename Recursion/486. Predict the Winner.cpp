3985ff31b4dda22029dc314bafcfb24488dbba61
Gaurav14cs17 <gouravkumar717@gmail.com>
2022-06-27 22:39:28 +0530

    recursion

--- Recursion/486. Predict the Winner.cpp
+++ Recursion/486. Predict the Winner.cpp
@@ -1,10 +1,14 @@
 /*
 https://leetcode.com/problems/predict-the-winner/
+soln :  https://leetcode.com/problems/predict-the-winner/discuss/2206053/Game-theory-(-uploaded-diagram-of-recursion-)
 */
+
 
 
 class Solution {
 public:
+
+    // Method_1
     int solve( vector<int>&nums , int s_idx , int e_idx   ){
       if( s_idx > e_idx )
           return 0;
@@ -16,21 +20,44 @@ public:
      return max( c_0 , c_1 );
     }
 
+    // Method 2
+
+    int dp[30][30][2];
+
+    int solve_2( vector<int>&nums , int i , int n , int player){
+        if( i > n ) return 0;
+
+        int ans = 0 ;
+        if(dp[i][n][player] != -1 )
+            return dp[i][n][player];
+
+        if(player){
+             int c_0 = nums[i] + solve_2( nums , i + 1  , n , player ^1 );
+             int c_1 = nums[n] + solve_2( nums , i  , n - 1 , player ^1 );
+             ans =  max( c_0 , c_1 );
+
+        }
+        else{
+             int c_0 = nums[i]*(-1) + solve_2( nums , i + 1  , n , player ^1 );
+             int c_1 = nums[n]*(-1) + solve_2( nums , i  , n - 1 , player ^1 );
+             ans =  min( c_0 , c_1 );
+        }
+
+        return dp[i][n][player] = ans ;
+
+
+    }
 
     bool PredictTheWinner(vector<int>& nums) {
        int n = nums.size()-1;
-       int ans = 0 ;
-       int total = 0;
-       for( auto it : nums)
-           total += it;
 
-       int player_1_value = solve( nums , 0 , n );
-       int player_2_value = total - player_1_value;
 
-       if(player_1_value >= player_2_value )
-           return true;
-        else
-            return false;
+       memset( dp , -1 , sizeof(dp));
+       int ans = solve_2( nums , 0  , n , 1 );
+       if( ans >=0 )
+           return 1;
+       else
+           return 0 ;
 
 
     }
